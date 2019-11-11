5d137dbd354f1ed00c330d0e472d646d1da8ffd2
Ghost <austin@Austinde-MacBook-Pro.local>
2019-11-11 16:37:39 +0800

    video

--- js/jsManage.js
+++ js/jsManage.js
@@ -116,3 +116,68 @@
 		$(document).on('touchmove touchstart touchend', function (e){});
 		
 	});
+
+	jQuery(function($){
+
+    // Lightbox Triggers
+    $(".open-video-link").videoBox();
+});
+/* ========================================================================= */
+/* FUNCTION TO CREATE LIGHTBOX */
+/* ========================================================================= */
+
+jQuery.fn.extend({
+    videoBox : function() {
+        var self, link, target, video, videoSrc, toggle;
+        this.each(function() {
+            self = this;
+            target = $(self).attr("href");
+            video = $(target).find(".popup-video iframe");
+            videoSrc = $(video).attr("src");
+          
+            $(this).on("click", function(event) {
+                event.preventDefault ? event.preventDefault() : event.returnValue = false;
+                $(target).wrap( "<div class='lightbox'></div>" );
+                $(".lightbox").fadeIn(300, function() {
+                    $(target).fadeIn(0);
+                    $("body").addClass("modal-open");
+                    $(video).attr("src",videoSrc+'?autoplay=1');
+                    resizeIfame(video);
+                });
+
+                $("body").on("click", function(event) {
+                    if(($(event.target).hasClass("lightbox") || $(event.target).hasClass("close")) && $(target).parent().hasClass("lightbox") ) {
+                        $(".lightbox").fadeOut(300, function() {
+                            $(target).hide(0);
+                            $(target).unwrap();
+                        });
+                        $("body").removeClass("modal-open");
+                        $(video).attr("src",videoSrc);
+                    }
+                });
+            });
+        });
+    }
+});
+
+
+/* ========================================================================= */
+/* RESIZE IFRAME VIDEO FUNCTION */
+/* ========================================================================= */
+function resizeIfame(frame) {
+
+    var oldWidth = $(frame).width();
+    var oldHeight = $(frame).height();
+    var propotion = oldHeight / oldWidth;
+    var newHeight;
+
+    $(frame).width('100%');
+    newHeight = $(frame).width() * propotion;
+    $(frame).height(newHeight);
+
+    $(window).resize(function() {
+        $(frame).width('100%');
+        newHeight = $(frame).width() * propotion;
+        $(frame).height(newHeight);
+    });
+}
